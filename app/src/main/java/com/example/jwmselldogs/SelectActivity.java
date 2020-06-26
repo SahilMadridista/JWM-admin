d2755feff5922737f28ac6524d32438c9e834d24
sahil <sahil91098singh@gmail.com>
2020-06-26 13:47:20 +0530

    Add item activity added.

--- app/src/main/java/com/example/jwmselldogs/SelectActivity.java
+++ app/src/main/java/com/example/jwmselldogs/SelectActivity.java
@@ -23,7 +23,7 @@ public class SelectActivity extends AppCompatActivity {
       doctor.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
-
+            startActivity(new Intent(getApplicationContext(),AddDoctorActivity.class));
          }
       });
 
